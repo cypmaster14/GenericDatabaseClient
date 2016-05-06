081e76400e0b6f42e85c065758b9d3238b5e80fa
cypmaster14 <ciprian.lazar95@gmail.com>
2016-05-06 23:09:48 +0300

    Add Graph bonus

--- src/com/UserGui.java
+++ src/com/UserGui.java
@@ -3,6 +3,8 @@ package com;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.FileNotFoundException;
@@ -14,6 +16,9 @@ import java.sql.SQLException;
 import javax.swing.JButton;
 import javax.swing.JDesktopPane;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -109,12 +114,26 @@ public class UserGui {
 		splitPane= new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,treeScroller,right);
 		splitPane.setResizeWeight(0.3f);
 		
+		JMenuBar menu = new JMenuBar();
+		JMenu graph= new JMenu("Graph");
+		menu.add(graph);
+		JMenuItem generateGraph = new JMenuItem("Generate Graph");
+		generateGraph.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				new Bonus(con);
+				
+			}
+		});
+		graph.add(generateGraph);
 		
 		
 		
 		
 		
-		
+		mainWindow.setJMenuBar(menu);
 		mainWindow.getContentPane().add(splitPane);		
 		mainWindow.setVisible(true);
 		
